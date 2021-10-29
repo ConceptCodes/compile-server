221ae2eb58946ad886feea060b28d1d5e1b86cc7
Concept <conceptcodes@gmail.com>
2021-10-29 18:45:58 -0400

    break needed, need new paraphrase api

--- routes/paraphrase.js
+++ routes/paraphrase.js
@@ -1,24 +1,49 @@
-const router = require('express').Router()
-const axios = require('axios').default
-require('dotenv').config()
+const router = require("express").Router();
+const axios = require("axios").default;
+require("dotenv").config();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  let article = req.body.article.match(/\(?[^\.\?\!]+[\.!\?]\)?/g)
+  let _article = []
+  let _strength = req.body.strength
+
+  article.forEach(sentence => {
     const options = {
-        method: 'POST',
-        url: 'https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite',
-        headers: {
-          'content-type': 'application/json',
-          'x-rapidapi-host': 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com',
-          'x-rapidapi-key': process.env.RAPID_API_KEY
-        },
-        data: {language: 'en', strength: req.body.strength, text: req.body.text}
-      };
-      
-      axios.request(options).then((response) => {
-          res.json(response.data)
-      }).catch((error) => {
-          console.error(error);
-      });
-})
+      method: "POST",
+      url:
+      "https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite",
+      headers: {
+        "content-type": "application/json",
+        "x-rapidapi-host":
+        "rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com",
+        "x-rapidapi-key": process.env.RAPID_API_KEY,
+      },
+      data: { language: "en", strength: _strength, text: sentence },
+    };
+    
+    axios
+    .request(options)
+    .then(async(response) =>  {
+      const data = await response.data
+      console.log(data)
+    })
+    .catch((error) => { console.error(error) });
+  });
+ 
+  
+  // calcualte score
+  // let _similarity = parseFloat(
+  //     _article
+  //       .map((li) => li.similarity)
+  //       .reduce((sum, val) => sum + val, 0) / _article.length
+  //   ).toFixed(2);
 
-module.exports = router
+    // convert back to string
+
+    // let _content = _article.map(x => x.rewrite)
+    console.log(_article)
+    
+    res.status(200).json({ content: '', similarity: 0 });
+});
+
+module.exports = router;
